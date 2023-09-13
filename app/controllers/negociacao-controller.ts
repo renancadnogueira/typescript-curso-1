61e7c76d76132719eecc75c5096316937f231ae0
Renan Cadamuro Nogueira <renan.cad.nogueira@gmail.com>
2023-09-13 11:29:35 -0300

    Inserção de mensagem View

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,22 +1,28 @@
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mensagem-view.js';
+import { NegociacoesView } from '../views/negociacoes-view.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement; //precisamos definir o tipo dessa classe, caso não seja nada definido, será definido o tipo any.
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+        this.mensagemView.update('Negociação adicionada com sucesso!');
+        this.negociacoesView.update(this.negociacoes); 
     }
 
     adiciona(): void { //tipo de retorno do método, quando não retorno nada é void
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
-        console.log(this.negociacoes.lista());
+        this.negociacoesView.update(this.negociacoes); //atualizando a view.
         this.limparFormulario();
     }
 
